259094b117473ff0104a361959e863da87af8cea
Nick Polishchuk <101862547+plsmrpostman@users.noreply.github.com>
2022-05-17 12:12:18 +0300

    initial commit

---
+++ precache-manifest.701a723fbb1b5732f6e3fa14ed231691.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1080bd56bec8e19f92",
+    "url": "/gifs-preview/css/app.d0e7c14f.css"
+  },
+  {
+    "revision": "2111e25731b23f733581",
+    "url": "/gifs-preview/css/chunk-vendors.e12e925f.css"
+  },
+  {
+    "revision": "af6ebf3feaef8a21f3e4bfc03cb18391",
+    "url": "/gifs-preview/img/logo.af6ebf3f.svg"
+  },
+  {
+    "revision": "ab6c6a76242aeb5a3f1b038a549d1bfa",
+    "url": "/gifs-preview/index.html"
+  },
+  {
+    "revision": "1f1080bd56bec8e19f92",
+    "url": "/gifs-preview/js/app.6c25345f.js"
+  },
+  {
+    "revision": "2111e25731b23f733581",
+    "url": "/gifs-preview/js/chunk-vendors.eba2ff5e.js"
+  },
+  {
+    "revision": "f4261a1581f20620c2bcbb62decadf2f",
+    "url": "/gifs-preview/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/gifs-preview/robots.txt"
+  }
+]);
